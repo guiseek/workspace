95b927f53cfc172b16c097abd8fe6fdc40a99844
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-11-04 20:41:21 -0300

    feat(card): desafios e comunidades

--- libs/card/bin/card.ts
+++ libs/card/bin/card.ts
@@ -1,21 +1,31 @@
 #!/usr/bin/env node
 
 import * as boxen from 'boxen';
+import { BorderStyle } from 'boxen';
 import * as chalk from 'chalk';
 
 const captains = console;
+const spc = (n = 1) => Array(n).fill(' ').join('');
 
 const data = {
   name: chalk.white('Guilherme Siquinelli'),
-  extra: chalk.gray('Degustador de tecnologia'),
+  challenge: [
+    chalk.gray('Entregar experiências'),
+    chalk.gray(`${spc(11)}atraentes e acessíveis`),
+  ],
   work: chalk.green('Engenheiro front-end'),
-  github: chalk.cyan('https://github.com/guiseek'),
-  twitter: chalk.cyan('https://twitter.com/guiseek'),
-  youtube: chalk.cyan('https://youtube.com/c/Guiseek'),
-  page: chalk.cyan('https://guiseek.dev'),
+  community: [
+    chalk.green('DevParaná'),
+    chalk.magenta(`${spc(11)}Hacktoberfest Brasil`)
+  ],
+  github: chalk.blue('https://github.com/guiseek'),
+  twitter: chalk.blue('https://twitter.com/guiseek'),
+  youtube: chalk.blue('https://youtube.com/c/Guiseek'),
+  page: chalk.blue('https://guiseek.dev'),
   npx: chalk.white('npx @guiseek/card'),
   labelName: chalk.white.bold('      Name:'),
-  labelExtra: chalk.white('     Extra:'),
+  labelChallenge: chalk.white('   Desafio:'),
+  labelCommunity: chalk.white('Comunidade:'),
   labelWork: chalk.white.bold('      Work:'),
   labelPage: chalk.white.bold('      Page:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
@@ -26,7 +36,8 @@ const data = {
 
 const newline = '\n';
 const heading = `${data.labelName} ${data.name}`;
-const handling = `${data.labelExtra} ${data.extra}`;
+const handling = `${data.labelChallenge} ${data.challenge[0]} \n ${data.challenge[1]}`;
+const community = `${data.labelCommunity} ${data.community[0]} \n ${data.community[1]}`;
 const working = `${data.labelWork} ${data.work}`;
 const pageing = `${data.labelPage} ${data.page}`;
 const githubing = `${data.labelGitHub} ${data.github}`;
@@ -40,6 +51,9 @@ const output =
   working +
   newline +
   handling +
+  newline +
+  newline +
+  community +
   newline +
   newline +
   pageing +
@@ -57,6 +71,7 @@ const card = chalk.green(
   boxen(output, {
     padding: 1,
     margin: 1,
+    borderStyle: BorderStyle.Round,
   })
 );
 
